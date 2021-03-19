b735fb07ac9dc0101110f970333043f61a3f7aa9
cagla01 <caglak770@gmail.com>
2021-03-19 18:06:18 +0100

    frontend

--- backend/config/db.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    HOST: "db.f4.htw-berlin.de",
-    USER: "s0573625",
-    PASSWORD: "buchliste",
-    DB: "_s0573625__webtech"
-};
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'htw-root',
+  selector: 'ck-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
